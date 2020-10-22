12db451cac4e9818cac7e541a222b2fe4c12af10
oleg <csharpdeveloper@list.ru>
2020-10-22 12:20:15 +0600

    modify ignore folders

--- config/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI : 'mongodb+srv://admin:GfiSZztHUS0YsNmT@cluster0.3kkys.mongodb.net/crm',
-    JWT: 'dev-jwt'
-}
--- config/keys.prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI = 'mongodb+srv://admin:GfiSZztHUS0YsNmT@cluster0.3kkys.mongodb.net/crm?retryWrites=true&w=majority',
-    JWT: 'dev-jwt'
-}
